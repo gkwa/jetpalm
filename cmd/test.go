4308d33007f585fb0839e92eb59d88a5f86c445e
Taylor Monacelli <taylormonacelli@gmail.com>
2024-10-26 09:33:44 -0700

    Add test

--- cmd/test.go
+++ cmd/test.go
@@ -21,9 +21,6 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("test called")
-		var cfg Config
-		out := cmd.OutOrStdout()
-		PrintConfigValues(out, &cfg)
 	},
 }
 
